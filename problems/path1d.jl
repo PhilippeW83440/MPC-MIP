c01126371fc5b721860c9902573ca4582b97f642
PhilippeW83440 <philippe.weingertner@gmail.com>
2020-05-26 22:11:32 +0200

    revert previous commit
    Dynamic Constraints were too loose ...

--- problems/path1d.jl
+++ problems/path1d.jl
@@ -161,9 +161,6 @@ end
 	dt = mpc.dt
 
 	ϵ = 0.25 # enlarge so we have a region to move in
-	ϵ = 3
-	# But anyways we are driving down the penalty to zero and we end up wit a max constraint violation
-	# of -6.880809166887047e-8
 
 	# Time Steps: 2 .. T
 	for k in range(1+mpc.nvars_dt, step=mpc.nvars_dt, length=mpc.T-1)
